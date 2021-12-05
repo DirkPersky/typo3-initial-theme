52e079dcc4326b6d782a02844ad4faf1e0047948
Dirk Persky <dirk.persky@gmail.com>
2021-12-05 22:11:30 +0100

    RSSBB SWUP init

---
+++ Resources/Public/js/template/rssbb.js
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021.
+ *
+ * @category TYPO3
+ *
+ * @copyright  2020 Dirk Persky (https://github.com/DirkPersky)
+ * @author     Dirk Persky <dirk.persky@gmail.com>
+ * @license    AGPL v3
+ */
+
+window.StateManager.attach('news-paging', function () {
+    // reinit
+    rrssbInit();
+});
