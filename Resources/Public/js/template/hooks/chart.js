c7c589cc0af9a9f9a70b22faea3ae1e01838604d
Dirk Persky <dirk.persky@gmail.com>
2022-01-24 19:17:52 +0100

    ChartJS Ajax Hook

--- Resources/Public/js/template/hooks/chart.js
+++ Resources/Public/js/template/hooks/chart.js
@@ -8,16 +8,6 @@
  * @license    AGPL v3
  */
 
-/*
- * Copyright (c) 2021.
- *
- * @category TYPO3
- *
- * @copyright  2020 Dirk Persky (https://github.com/DirkPersky)
- * @author     Dirk Persky <dirk.persky@gmail.com>
- * @license    AGPL v3
- */
-
 (function (Hoogi91) {
     // stop from running again, if accidently included more than once.
     if (typeof Hoogi91.hasInitialised != 'undefined') return
