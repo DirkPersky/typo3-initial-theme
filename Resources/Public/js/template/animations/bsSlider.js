79df0e116ebfc1a7db44f10aefc5b1e77867b90e
Dirk.Persky <dirk.persky@gmail.com>
2022-06-22 08:43:01 +0200

    Template Update

--- Resources/Public/js/template/animations/bsSlider.js
+++ Resources/Public/js/template/animations/bsSlider.js
@@ -9,52 +9,53 @@
  */
 
 window.AnimateManager.attach('bsSlider', function () {
-    return; // disable for the moment
-    var headerSlider = document.querySelectorAll('.carousel');
+    return true;
+    var headerSlider = document.querySelectorAll('.carousel')
+    AnimationStagger = 0.02,
+        AnimationTarget = '.carousel-caption p';
     // loop sliders
     headerSlider.forEach(slider => {
         var items = slider.querySelectorAll('.carousel-inner .carousel-item'),
             carousel = new bootstrap.Carousel(slider);
         // transition delay
         items.forEach((e, i) => {
-            e.style.transitionDelay = '.6s';
+            // // items to animate
+            var items = e.querySelectorAll(AnimationTarget);
+            // set new Delay for item
+            e.style.transitionDelay = (AnimationStagger * (items.length + 1) + 0.6)+'s';
         });
         // bind out trigger
         slider.addEventListener('slide.bs.carousel', (event) => {
+            // items to animate
+            var items = slider.querySelector('.carousel-inner .carousel-item.active').querySelectorAll(AnimationTarget);
             // out animation
-            onLeave(slider.querySelectorAll('.carousel-inner .carousel-item.active .carousel-caption'));
-            // prepare next item
-            event.relatedTarget.querySelectorAll('.carousel-caption').forEach(e => e.style.visibility = 'hidden');
+            onLeave(items);
         });
         // bind in trigger
         slider.addEventListener('slid.bs.carousel', (event) => {
             carousel.pause();
             carousel.cycle();
+            // items to animate
+            var items = event.relatedTarget.querySelectorAll(AnimationTarget);
             // start in animation
-            onEnter(event.relatedTarget.querySelectorAll('.carousel-caption'));
+            onEnter(items);
         });
-
         // on enter animation
         function onEnter(element) {
-            // remove visibilty of old element
-            element.forEach(e => e.style.removeProperty('visibility'));
-            // get left col Header
-            var sliderText = new SplitType(element, {types: "words, chars"});
             // inimate Text
-            window.DPAnimate.animate(sliderText.chars, {
-                class: 'animated textBounceLeft',
-                stagger: 0.02,
-                onComplete: e => sliderText.revert()
+            window.DPAnimate.animate(element, {
+                class: 'animated fadeInRight',
+                stagger: AnimationStagger,
+                onItemComplete: e => e.style.visibility = 'visible'
             });
         }
-
         // out enter animation
         function onLeave(element) {
             // inimate Text
             window.DPAnimate.animate(element, {
-                class: 'animated fadeOut',
-                stagger: 0.02,
-                onComplete: e => e.style.visibility = 'hidden'
+                class: 'animated fadeOutDown',
+                stagger: AnimationStagger,
+                onItemComplete: e => e.style.visibility = 'hidden'
             });
         }
     });
