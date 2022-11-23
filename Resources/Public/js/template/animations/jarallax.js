d50db009eae33c99ae1ed7ffc0ada6f2948a3780
dirk.persky <d.persky@gutenberghaus.de>
2022-11-23 16:25:43 +0100

    Parallax Effekt updates

--- Resources/Public/js/template/animations/jarallax.js
+++ Resources/Public/js/template/animations/jarallax.js
@@ -10,21 +10,7 @@
 //
 window.AnimateManager.attach('jarallax-effekts', function () {
     // get Slider image
-    u('[data-parallax]').map(e => {
-        // get selector
-        var dataSelector = e.dataset.parallax;
-        // remove datset
-        delete e.dataset.parallax;
-        // prepare for jarallax
-        e.querySelectorAll(dataSelector).forEach(item => {
-            item.classList.add('jarallax');
-            item.querySelectorAll('img').forEach(img => {
-                img.classList.add('jarallax-img');
-            });
-            // init jarallax for item
-            jarallax(item, {});
-        });
-    });
+    jarallax(document.querySelectorAll('.carousel .jarallax'), {});
 
     // init jarallax for css elements
     jarallax(document.querySelectorAll('.css--element.jarallax'), {
